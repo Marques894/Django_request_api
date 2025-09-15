7768e3a37d96da0988ea898d54c346bf9e40576f
Marques894 <renanmarques894@gmail.com>
2025-09-14 21:04:33 -0300

    Request advice api

--- chucknurris_jokes/core/views.py
+++ chucknurris_jokes/core/views.py
@@ -1,6 +1,13 @@
-from django.http import HttpRequest
+from django.http import HttpResponse
+import requests
 
 # Create your views here.
 
+def do_a_request():
+    URL = "https://api.adviceslip.com/advice"
+    req = requests.get(URL)
+    response = req.json()
+    return response["slip"]["advice"]
+
 def chucknurris(request):
-    return HttpRequest("")
+    return HttpResponse(do_a_request())
